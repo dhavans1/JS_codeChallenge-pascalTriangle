fd5969c4ef1028c8ab372a21a82d1815312c35ad
dhavans1 <dhavan.shekarappa@gmail.com>
2019-09-09 19:13:27 -0400

    initial commit

---
+++ pascalTriangle.js
@@ -0,0 +1,45 @@
+//Factorial function
+function getFactorial(x){
+
+    return x == 0 ? 1 : x * getFactorial(x-1);
+}
+
+//Pascal triangle function
+function printPascalTriangle(rows){
+       
+    //Loop for rows - n
+    for( n=0; n<rows; n++ ){        
+        // init string to store triangle row values
+        var pascalRow = "";
+        //Loop for columns - k
+        for( k=0; k<=n; k++ ){
+            //Implement nCk formula - n!/(k!*(n-k)!)
+            pascalRow += getFactorial(n)/(getFactorial(k) * getFactorial(n-k)) + ' ';
+        }
+        
+        console.log(pascalRow);
+    }
+}
+
+//Get Process' standard input object for user input
+var std_in = process.stdin;
+
+//Set character encoding
+std_in.setEncoding('utf-8');
+
+//Prompt user for n (Pascal triangle rows)
+console.log("Enter number of rows for Pascal triangle(n). Hit Ctrl+C to exit");
+
+std_in.on('data', (rows) =>{
+
+    //Validate user input
+    if( isNaN(parseInt(rows)))
+        console.log('Invalid entry. Please enter a number');
+    else if( rows < 0)
+            console.log('Invalid entry. Please enter non-negative number(K should be positive)');
+         else{
+            console.log('Pascal triangle with '+ parseInt(rows) +' rows');
+            printPascalTriangle(parseInt(rows));
+            console.log("Enter number of rows for Pascal triangle(n). Hit Ctrl+C to exit");
+         }
+})
